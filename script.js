e0a2b2e404fab534565041ec72aee133aa80f007
Joshua Devon <joshua.devon@gmail.com>
2024-04-28 17:24:25 +1000

    16x16 div grid created via JS function.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("It's working.")
+const boxContainer = document.getElementById("boxes");
+
+function makeBoxes() {
+    for (let j = 0; j < 16; j++) {
+        for (let i = 0; i < 16; i++) {
+            let boxo = document.createElement("div");
+            boxo.className = "boxo";
+            boxContainer.append(boxo);
+        }
+    }
+}
+
+makeBoxes();
